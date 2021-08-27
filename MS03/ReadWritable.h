66717c1e4e7b483e05b50b69ba0d71b965e7b9aa
rafialarabi <84462357+rafialarabi@users.noreply.github.com>
2021-08-27 02:52:55 -0400

    Add files via upload

---
+++ MS03/ReadWritable.h
@@ -0,0 +1,39 @@
+
+// Name:           Md Rafi Al Arabi Bhuiyan
+// Student Number: 147307193
+// Email:          mraabhuiyan@myseneca.ca
+// Section:        NBB
+// Milestone 3
+// Date:           22/07/2020
+
+/* I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments. */
+
+
+#ifndef SDDS_READWRITABLE_H
+#define SDDS_READWRITABLE_H
+
+#include <iostream>
+#include <string>
+
+
+namespace sdds {
+
+	class ReadWritable {
+
+		bool choice;
+
+	public:
+		ReadWritable();
+		~ReadWritable();
+		bool isCsv() const;
+		void setCsv(bool value);
+		virtual std::istream& read(std::istream&) = 0;
+		virtual std::ostream& write(std::ostream&) const = 0;
+	};
+	std::istream& operator>>(std::istream&, ReadWritable&);
+	std::ostream& operator<<(std::ostream&, const ReadWritable&);
+
+}
+
+#endif // !SDDS_READWRITABLE_H
